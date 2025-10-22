3a861bb83af9c0e6281bb404dc876aafff22222d
Wenwu <artemiip427@gmail.com>
2025-10-22 18:55:27 +0300

    add script in classwork-22.10

---
+++ classwork-2025/work-22.10/work.py
@@ -0,0 +1,13 @@
+num = int(input("Введите число: "))
+
+def days_to_word(number):
+    if number == 1:
+        return "день"
+    elif number % 10 == 1 and number != 11:
+        return "день"
+    elif number % 10 in range(2, 5) and number not in range(12, 15):
+        return "дня"
+    else:
+        return "дней"
+    
+print(days_to_word(num))
